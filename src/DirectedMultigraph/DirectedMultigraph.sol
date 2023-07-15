cf5ed0bb7a7ef432d26ab278ba42743d48085299
Conner Swenberg <cls364@cornell.edu>
2023-07-15 02:06:31 -0700

    Add DirectedMultigraph implementation

---
+++ src/DirectedMultigraph/DirectedMultigraph.sol
@@ -0,0 +1,129 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ERC1155} from "lib/openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
+import {IDirectedMultigraph} from "./IDirectedMultigraph.sol";
+
+/// @notice Opinionated extension of ERC1155 for modeling graph problems via [directed multigraphs](https://en.wikipedia.org/wiki/Multigraph).
+///         Potentially useful for endorsement graphs like Coordinape or other complex multiplayer games.
+contract DirectedMultigraph is ERC1155, IDirectedMultigraph {
+    constructor(string memory uri_) ERC1155(uri_) {}
+
+    // metadata
+
+    function graphURI(uint96 graphId) external view virtual returns (string memory) {}
+
+    function edgeURI(uint96 graphId, address distributor, address owner)
+        external
+        view
+        virtual
+        returns (string memory)
+    {}
+
+    function nodeURI(uint96 graphId, address node) external view returns (string memory) {
+        return uri(GraphEncoding.encodeNode(graphId, node));
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(IDirectedMultigraph).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    // flip approval operator to distributors, not owners
+
+    function safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes memory data)
+        public
+        override
+    {
+        (, address distributor) = GraphEncoding.decodeNode(id);
+        require(
+            distributor == _msgSender() || isApprovedForAll(distributor, _msgSender()),
+            "ERC1155: caller is not token distributor or approved" // changed "owner" -> "distributor"
+        );
+        _safeTransferFrom(from, to, id, amount, data);
+    }
+
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public virtual override {
+        uint256 len = ids.length;
+        for (uint256 i; i < len; i++) {
+            (, address distributor) = GraphEncoding.decodeNode(ids[i]);
+            // not gas optimized, but clear and it works for POC
+            require(
+                distributor == _msgSender() || isApprovedForAll(distributor, _msgSender()),
+                "ERC1155: caller is not token distributor or approved" // changed "owner" -> "distributor"
+            );
+        }
+        _safeBatchTransferFrom(from, to, ids, amounts, data);
+    }
+
+    // convenience parity functions
+
+    function balanceOf(uint96 graphId, address distributor, address owner)
+        public
+        view
+        virtual
+        returns (uint256 amount)
+    {
+        return balanceOf(owner, GraphEncoding.encodeNode(graphId, distributor));
+    }
+
+    function balanceOfBatch(uint96[] memory graphIds, address[] memory distributors, address[] memory accounts)
+        public
+        view
+        virtual
+        returns (uint256[] memory amounts)
+    {
+        return balanceOfBatch(accounts, GraphEncoding.encodeNodes(graphIds, distributors));
+    }
+
+    function safeTransferFrom(
+        uint96 graphId,
+        address distributor,
+        address from,
+        address to,
+        uint256 amount,
+        bytes memory data
+    ) public virtual {
+        safeTransferFrom(from, to, GraphEncoding.encodeNode(graphId, distributor), amount, data);
+    }
+
+    function safeBatchTransferFrom(
+        uint96[] memory graphIds,
+        address[] memory distributors,
+        address from,
+        address to,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public virtual {
+        safeBatchTransferFrom(from, to, GraphEncoding.encodeNodes(graphIds, distributors), amounts, data);
+    }
+}
+
+library GraphEncoding {
+    function decodeNode(uint256 tokenId) public pure returns (uint96 graphId, address node) {
+        graphId = uint96(tokenId >> 160);
+        node = address(uint160(tokenId));
+    }
+
+    function encodeNode(uint96 graphId, address node) public pure returns (uint256 tokenId) {
+        tokenId = (uint256(graphId) << 160) | uint256(uint160(node));
+    }
+
+    function encodeNodes(uint96[] memory graphIds, address[] memory distributors)
+        public
+        pure
+        returns (uint256[] memory ids)
+    {
+        uint256 len = graphIds.length;
+        require(len == distributors.length);
+        ids = new uint256[](len);
+        for (uint256 i; i < len; i++) {
+            ids[i] = encodeNode(graphIds[i], distributors[i]);
+        }
+    }
+}
--- src/DirectedMultigraph/IDirectedMultigraph.sol
+++ src/DirectedMultigraph/IDirectedMultigraph.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-// A wrapper around ERC1155 to model [directed multigraphs](https://en.wikipedia.org/wiki/Multigraph)
+/// @notice Opinionated extension of ERC1155 for modeling graph problems via [directed multigraphs](https://en.wikipedia.org/wiki/Multigraph).
+///         Potentially useful for endorsement graphs like Coordinape or other complex multiplayer games.
 interface IDirectedMultigraph {
+    // metadata
+
     // returns uri of graph metadata
     function graphURI(uint96 graphId) external returns (string memory);
 
@@ -10,20 +13,40 @@ interface IDirectedMultigraph {
     function edgeURI(uint96 graphId, address distributor, address owner) external returns (string memory);
 
     // returns uri of node metadata on a graph
-    // equivalent with tokenURI()
+    // equivalent with ERC1155.uri(id)
     function nodeURI(uint96 graphId, address node) external returns (string memory);
 
-    // returns edge value
-    // equivalent with balanceOf(tokenId, owner)
-    function balanceOf(uint96 graphId, address distributor, address owner) external returns (uint256 value);
+    // convenience parity functions
 
-    // safeTransferFrom is not controlled by "owner"/to-node, but from-node
-    // equivalent with safeTransferFrom(tokenId, from, to)
-    function safeTransferFrom(uint96 graphId, address distributor, address from, address to, uint256 value)
+    // returns edge amount
+    // equivalent with ERC1155.balanceOf(tokenId, owner)
+    function balanceOf(uint96 graphId, address distributor, address owner) external returns (uint256 amount);
+
+    // returns edge values in batch
+    // equivalent with ERC1155.balanceOfBatch(owners, ids)
+    function balanceOfBatch(uint96[] memory graphIds, address[] memory distributors, address[] memory owners)
         external
-        returns (bool);
+        returns (uint256[] memory amounts);
 
-    // library, tokenId = graphId + node
-    function decodeNode(uint256 tokenId) external returns (uint96 graphId, address node);
-    function encodeNode(uint96 graphId, address node) external returns (uint256 tokenId);
+    // transfers are not controlled by "owner", but by "distributor" in graphs
+    // equivalent with ERC1155.safeTransferFrom(tokenId, from, to, amount, data)
+    function safeTransferFrom(
+        uint96 graphId,
+        address distributor,
+        address from,
+        address to,
+        uint256 amount,
+        bytes memory data
+    ) external;
+
+    // transfers are not controlled by "owner", but by "distributor" in graphs
+    // equivalent with ERC1155.safeTransferFromBatch(from, to, ids, amounts, data)
+    function safeBatchTransferFrom(
+        uint96[] memory graphIds,
+        address[] memory distributors,
+        address from,
+        address to,
+        uint256[] memory amounts,
+        bytes memory data
+    ) external;
 }
